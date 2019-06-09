8a8783c1d96b34322f99e9acb88f0e92a94e9d38
Luis Garcia <garcia50@yahoo.com>
2019-06-09 16:16:13 -0400

    Add overlay feature

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,9 @@
 const body = $('body')
 const usersURL = 'https://randomuser.me/api/?results=12' 
 const galleryDiv = $('.gallery')
-const modalDivContainter = document.createElement('div');
-// const modalDiv = document.createElement('div');
-// const modalInfoDiv = document.createElement('div');
-// const modalCloseBtn = document.createElement('button');
-
-modalDivContainter.class = 'modal-container';
-// modalDiv.class = 'modal';
-// modalInfoDiv.class = 'modal-info-container';
-// modalCloseBtn.type = 'button';
-// modalCloseBtn.class = 'modal-close-btn';
-// modalCloseBtn.id = 'modal-close-btn';
-// modalCloseBtn.innerHTML = '<strong>X</strong>'
+var usersArray;
+// galleryDiv.append(modalDivContainter);
+// body.prepend(modalDivContainter);
 
 
 function fetchData(url) {
@@ -27,7 +18,7 @@ Promise.all([
   fetchData(usersURL)
 ])
 .then(data => {
-  const usersArray = data[0].results;
+  usersArray = data[0].results;
   generateUser(usersArray);
 })
 
@@ -42,66 +33,67 @@ function checkStatus(response) {
 
 
 function generateUser(data) {
-  const users = data.map(user => `
-    <div class="card">
-      <div class="card-img-container">
-          <img class="card-img" src=${user.picture.large} alt="profile picture">
+  const users = data.map((user, index) => `
+    <div class="card" usernum="${index}">
+      <div class="card-img-container" usernum="${index}">
+          <img class="card-img" usernum="${index}" src=${user.picture.large} alt="profile picture">
       </div>
-      <div class="card-info-container">
-          <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last} </h3>
-          <p class="card-text">${user.email}</p>
-          <p class="card-text cap">${user.location.city}</p>
+      <div class="card-info-container" usernum="${index}">
+          <h3 id="name" class="card-name cap" usernum="${index}">${user.name.first} ${user.name.last} </h3>
+          <p class="card-text" usernum="${index}">${user.email}</p>
+          <p class="card-text cap" usernum="${index}">${user.location.city}</p>
       </div>
     </div>
   `).join('');
 
+
   galleryDiv.append(users);
-  console.log(data);
 }
 
-/*Create a modal window
-  When any part of an employee item in the directory is clicked, a modal window should pop up with the following details displayed:
-    Image
-    Name
-    Email
-    City or location
-    Cell Number
-    Detailed Address, including street name and number, state or country, and post code.
-    Birthday
-Make sure there’s a way to close the modal window
-Refer to the mockups and the comments in the index.html file for an example of what info should be displayed on the page and how it should be styled.*/
+
 
 galleryDiv.on('click', function(e) {
-  console.log(e.target.firstElementChild.firstElementChild.src);
-  const userModal = `
-    <div class="modal">
-      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-      <div class="modal-info-container">
-          <img class="modal-img" src=${e.target.firstElementChild.firstElementChild.src} alt="profile picture">
-          <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last} </h3>
-          <p class="modal-text">${user.email}</p>
-          <p class="modal-text cap">${user.location.city}</p>
-          <hr>
-          <p class="modal-text">${user.cell}</p>
-          <p class="modal-text">${user.street} ${user.city} ${user.state} ${user.postcode}</p>
-          <p class="modal-text">Birthday: ${user.dob.date}</p>
+  console.log('hey', usersArray);
+
+  console.log(e.target.attributes);
+
+  var user = e.target.attributes.usernum.value || '';
+
+  var picture = usersArray[user].picture.large
+  var first = usersArray[user].name.first;
+  var last = usersArray[user].name.last;
+  var email = usersArray[user].email;
+  var phone = usersArray[user].phone;
+  var street = usersArray[user].location.street;
+  var city = usersArray[user].location.city;
+  var state = usersArray[user].location.state;
+  var zip = usersArray[user].location.postcode;
+  var dob = usersArray[user].dob;
+
+  if (user != '') {
+    var userModal = `
+      <div class="modal-container">
+        <div class="modal">
+          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+          <div class="modal-info-container">
+              <img class="modal-img" src=${picture} alt="profile picture">
+              <h3 id="name" class="modal-name cap">${first} ${last}</h3>
+              <p class="modal-text">${email}</p>
+              <p class="modal-text cap">${city}</p>
+              <hr>
+              <p class="modal-text">${phone}</p>
+              <p class="modal-text">${street} ${city}, ${state} ${zip}</p>
+              <p class="modal-text">Birthday: ${dob}</p>
+          </div>
+        </div>
       </div>
-  </div>
-  `
+    `
 
-  modalDivContainter.append(userModal);
-
-  console.log(e.target);
+    body.append($(userModal))
+  }
 
 
 })
-
-
-
-
-
-
-
 
 
 
